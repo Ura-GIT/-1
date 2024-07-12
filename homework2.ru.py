13b613a868ad21edd58cdd71a885583542e3890f
Ura-GIT <Jameskenyay@gmail.com>
2024-07-12 17:27:55 +0300

    Add files via upload

---
+++ homework2.ru.py
@@ -0,0 +1,10 @@
+first_name ='Python'
+print(first_name)
+number_of_hours_spent=12
+print(number_of_hours_spent)
+Number_of_completed= 1.5
+print(Number_of_completed)
+print(Number_of_completed / number_of_hours_spent)
+time_for_one_task=0.125
+print( 'Курс :' , first_name,'всего задач :',number_of_hours_spent ,'затрачено часов :',Number_of_completed, 'среднее время выполнения',time_for_one_task)
+
