1609c75c5e5ca53c6b2e652f86c1d83df6925a11
Ura-GIT <Jameskenyay@gmail.com>
2024-07-20 16:41:59 +0300

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -1,24 +1,19 @@
 first = int(input("число 123: "))
 second = int(input("число 456: "))
 third = int(input("число 789: "))
-
 if first == second == third:
     print(3)
+elif first == second or second == third or first == third:
+    print(2)
 else:
-    if first == third or first == second:
-        print(2)
-    if first != second != third:
-        print(0)
+    print(0)
 
 
-first = int(input("число 42: "))
-second = int(input("число 69: "))
-third = int(input("число 42: "))
 
-if first == second == third:
-    print(3)
-else:
-    if first == third or first == second:
-        print(2)
-    if first != second != third:
-        print(0)
+
+
+
+
+
+
+
