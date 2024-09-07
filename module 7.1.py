3e8c8a03ef89e7dc3e0452fcfe6215b322766667
Ura-GIT <Jameskenyay@gmail.com>
2024-09-07 16:35:04 +0300

    Add files via upload

---
+++ module 7.1.py
@@ -0,0 +1,41 @@
+# ключ и значение разделяется знаком :
+# get
+#pop - вытаскивает из словаря  ключ  и значение
+# keys -достает список из ключей в словаре
+# values- достает из списка значение словаря
+# items - достает и список и значение
+# на месте ключ не изменяемые обьекты тоесть нельзя ввести давнные ,а в значении можно добавлять
+#  set_       множество хранит в себе уникальное значение  множества не содержат поворяющие элементы
+# add добавление элемента во множество
+
+while 1 > 0:
+    my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+    n = 0
+    if my_list[0] >= n:
+        print(my_list[0])
+        if my_list[1] >= n:
+            print(my_list[1])
+            if my_list[2] >= n:
+                print(my_list[2])
+                if my_list[3] >= n:
+                    print(my_list[3])
+                    if my_list[4] >= n:
+                        print(my_list[4])
+                        if my_list[5] >= n:
+                            print(my_list[5])
+                            if my_list[6] >= n:
+                                print(my_list[6])
+                                if my_list[7] >= n:
+                                    print(my_list[7])
+                                    if my_list[8] >= n:
+                                        print(my_list[8])
+                                        if my_list[9] >= n:
+                                            print(my_list[9])
+                                            if my_list[10] >= n:
+                                                print(my_list[10])
+                                                if my_list[11] >= n:
+                                                    print(my_list[11])
+                                                    if my_list[12] >= n:
+                                                        print(my_list[12])
+                                break
+    else:
